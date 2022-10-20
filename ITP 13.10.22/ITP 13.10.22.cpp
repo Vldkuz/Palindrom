b5a0e4df7b7a0c29184d8f973fda4a1218c0147e
Vladislav <kuznetsov230304@gmail.com>
2022-10-21 00:13:43 +0500

    Добавьте файлы проекта.

---
+++ ITP 13.10.22/ITP 13.10.22.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	int k = n;
+	auto counter_r=0;
+	while (n > 0)
+	{
+		counter_r += 1;
+		n /= 10;
+	}
+
+	int i = 1;
+	while (k>0)
+	{
+		int t = k / pow(10, counter_r - 1);
+		int m = k % int(pow(10, 1));
+
+		if (t != m)
+		{
+			cout << "N\n";
+			return 0;
+		}
+
+		k = k % int(pow(10, counter_r - 1));
+		k = k / 10;
+		counter_r -= 2;
+	}
+
+	cout << "Y\n" << endl;
+	return 0;
+}
